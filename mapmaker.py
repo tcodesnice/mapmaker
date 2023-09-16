f67b09d1bc4d2bfc8eae9c5efe07588d08a71ce7
tcodesnice <129119381+tcodesnice@users.noreply.github.com>
2023-09-16 13:57:21 -0700

    bug fix

--- mapmaker.py
+++ mapmaker.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import folium
 
-csv_file_path = 'insert/file/path/here.csv'
+csv_file_path = 'enter/path/to/csv/file.csv'
 data = pd.read_csv(csv_file_path)
 
 # Process latitude and longitude values with directions
 def process_coordinates(coord_with_direction):
     numeric_part = float(coord_with_direction[:-1])
     direction = coord_with_direction[-1]
-    if direction in ['N', 'E']:
-        return numeric_part
-    elif direction in ['S', 'W']:
+    if direction in ['S', 'W']:
         return -numeric_part
     else:
-        return None
+        return numeric_part
 
-data['Latitude'] = data['Latitude'].apply(process_coordinates)
-data['Longitude'] = data['Longitude'].apply(process_coordinates)
+# Apply the process_coordinates function only to rows with directions
+data['Latitude'] = data['Latitude'].apply(lambda x: process_coordinates(x) if isinstance(x, str) else x)
+data['Longitude'] = data['Longitude'].apply(lambda x: process_coordinates(x) if isinstance(x, str) else x)
 
 # Create a base map centered at a certain location
 map_center = [data['Latitude'].mean(), data['Longitude'].mean()]
@@ -27,6 +26,5 @@ for index, row in data.iterrows():
     folium.Marker([row['Latitude'], row['Longitude']]).add_to(my_map)
 
 # Save the map as an HTML file
-map_file_path = 'insert/file/path/here.html'
+map_file_path = 'output/file/path.html'
 my_map.save(map_file_path)
-
